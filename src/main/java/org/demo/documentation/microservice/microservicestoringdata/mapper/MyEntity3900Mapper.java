187442ee62dd37d43cd5e692b1a9c1ab88998001
agelfman <agelfman@cxlab.ru>
2024-04-19 15:36:24 +0300

    CXBOX-261 description microservices

--- src/main/java/org/demo/documentation/microservice/microservicestoringdata/mapper/MyEntity3900Mapper.java
+++ src/main/java/org/demo/documentation/microservice/microservicestoringdata/mapper/MyEntity3900Mapper.java
@@ -5,6 +5,7 @@ import org.demo.documentation.microservice.microservicestoringdata.dto.MyExample
 import org.demo.documentation.microservice.microservicestoringdata.repository.entity.MyEntity3900;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface MyEntity3900Mapper {
@@ -13,5 +14,13 @@ public interface MyEntity3900Mapper {
 	@Mapping(target = "customField", source = "myEntity3900.customField")
 
 	MyExample3900DTO toDto(final MyEntity3900 myEntity3900);
+
+	@Mapping(target = "id")
+	@Mapping(target = "customField")
+	MyEntity3900 newEntityByDto(final String id, final MyExample3900DTO request);
+
+	@Mapping(target = "id")
+	@Mapping(target = "customField")
+	MyEntity3900 updateEntityByDto(@MappingTarget final MyEntity3900 myEntity3900, final MyExample3900DTO request);
 
 }
