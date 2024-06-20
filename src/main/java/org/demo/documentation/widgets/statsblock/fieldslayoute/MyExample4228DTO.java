a0e0781faf85560dc5a1649e102f4ebe9f3cbe3d
avgelfman <143416914+avgelfman@users.noreply.github.com>
2024-06-20 11:27:00 +0300

    CXBOX-207 statistic blocks (#113)

---
+++ src/main/java/org/demo/documentation/widgets/statsblock/fieldslayoute/MyExample4228DTO.java
@@ -0,0 +1,25 @@
+package org.demo.documentation.widgets.statsblock.fieldslayoute;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.cxbox.api.data.dto.DataResponseDTO;
+
+// 4228
+// 42280
+// 4228
+// 4226
+@Getter
+@Setter
+@NoArgsConstructor
+public class MyExample4228DTO extends DataResponseDTO {
+    private String title;
+
+    private Long value;
+
+    private String color;
+
+    private String description;
+
+    private String icon;
+}
