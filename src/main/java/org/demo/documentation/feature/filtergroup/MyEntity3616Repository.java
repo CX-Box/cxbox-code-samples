69a9307af56f66464cd98a75f49f64b09c87bb11
avgelfman <143416914+avgelfman@users.noreply.github.com>
2024-07-05 16:43:26 +0300

    CXBOX- 93 Documentation. Filter Group (#125)

---
+++ src/main/java/org/demo/documentation/feature/filtergroup/MyEntity3616Repository.java
@@ -0,0 +1,10 @@
+package org.demo.documentation.feature.filtergroup;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyEntity3616Repository extends JpaRepository<MyEntity3616, Long>, JpaSpecificationExecutor<MyEntity3616> {
+
+}
---
+++ src/main/java/org/demo/documentation/feature/filtergroup/MyExample3616Meta.java
@@ -0,0 +1,26 @@
+package org.demo.documentation.feature.filtergroup;
+
+import org.cxbox.core.crudma.bc.impl.InnerBcDescription;
+import org.cxbox.core.dto.rowmeta.FieldsMeta;
+import org.cxbox.core.dto.rowmeta.RowDependentFieldsMeta;
+import org.cxbox.core.service.rowmeta.FieldMetaBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyExample3616Meta extends FieldMetaBuilder<MyExample3616DTO> {
+
+    @Override
+    public void buildRowDependentMeta(RowDependentFieldsMeta<MyExample3616DTO> fields, InnerBcDescription bcDescription,
+                                      Long id, Long parentId) {
+        fields.setEnabled(MyExample3616DTO_.customFieldNew);
+        fields.setEnabled(MyExample3616DTO_.customField);
+        fields.setRequired(MyExample3616DTO_.customField);
+    }
+
+    @Override
+    public void buildIndependentMeta(FieldsMeta<MyExample3616DTO> fields, InnerBcDescription bcDescription, Long parentId) {
+        fields.enableFilter(MyExample3616DTO_.customFieldNew);
+        fields.enableFilter(MyExample3616DTO_.customField);
+    }
+
+}
