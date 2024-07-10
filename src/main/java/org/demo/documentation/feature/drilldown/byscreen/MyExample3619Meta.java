a6d99514060a8c3390acf646b18facc9f2c2137b
avgelfman <143416914+avgelfman@users.noreply.github.com>
2024-07-10 14:59:28 +0300

    CXBOX- 322 Documentation. Drilldown (#129)

---
+++ src/main/java/org/demo/documentation/feature/drilldown/byscreen/MyExample3619Meta.java
@@ -0,0 +1,31 @@
+package org.demo.documentation.feature.drilldown.byscreen;
+
+import org.cxbox.core.crudma.bc.impl.InnerBcDescription;
+import org.cxbox.core.dto.DrillDownType;
+import org.cxbox.core.dto.rowmeta.FieldsMeta;
+import org.cxbox.core.dto.rowmeta.RowDependentFieldsMeta;
+import org.cxbox.core.service.rowmeta.FieldMetaBuilder;
+ 
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyExample3619Meta extends FieldMetaBuilder<MyExample3619DTO> {
+
+    @Override
+    public void buildRowDependentMeta(RowDependentFieldsMeta<MyExample3619DTO> fields, InnerBcDescription bcDescription,
+                                      Long id, Long parentId) {
+        fields.setEnabled(MyExample3619DTO_.customField);
+        fields.setRequired(MyExample3619DTO_.customField);
+
+        fields.setDrilldown(
+                MyExample3619DTO_.customField,
+                DrillDownType.INNER,
+                "/screen/myexample3610"
+        );
+    }
+
+    @Override
+    public void buildIndependentMeta(FieldsMeta<MyExample3619DTO> fields, InnerBcDescription bcDescription, Long parentId) {
+    }
+
+}
