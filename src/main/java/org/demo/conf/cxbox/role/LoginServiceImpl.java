7d0ab3fa326903a85ccbc39722f79e946246550d
agelfman <agelfman@cxlab.ru>
2024-07-19 16:18:37 +0300

    CXBOX-327 New parameter filtration by range for Date/DateTime/DateTimeWithSeconds

--- src/main/java/org/demo/conf/cxbox/role/LoginServiceImpl.java
+++ src/main/java/org/demo/conf/cxbox/role/LoginServiceImpl.java
@@ -37,6 +37,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 @Slf4j
@@ -45,78 +46,80 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class LoginServiceImpl implements LoginService {
 
-	private final SessionService sessionService;
+    private final SessionService sessionService;
 
-	private final CoreSessionService coreSessionService;
+    private final CoreSessionService coreSessionService;
 
-	private final UserRoleService userRoleService;
+    private final UserRoleService userRoleService;
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	private final ScreenResponsibilityService screenResponsibilityService;
+    private final ScreenResponsibilityService screenResponsibilityService;
 
-	private final MetaConfigurationProperties metaConfigurationProperties;
+    private final MetaConfigurationProperties metaConfigurationProperties;
 
-	private final UIProperties uiProperties;
+    private final UIProperties uiProperties;
 
-	/**
-	 * Build info for active session user for specific role
-	 *
-	 * @param role Requested role
-	 * @return LoggedUser User info with settings, supported features, locale and time zone
-	 */
-	@Override
-	@SneakyThrows
-	public LoggedUser getLoggedUser(String role) {
+    /**
+     * Build info for active session user for specific role
+     *
+     * @param role Requested role
+     * @return LoggedUser User info with settings, supported features, locale and time zone
+     */
+    @Override
+    @SneakyThrows
+    public LoggedUser getLoggedUser(String role) {
 
-		setSessionUserInternalRole(role);
+        setSessionUserInternalRole(role);
 
-		IUser<Long> user = sessionService.getSessionUser();
-		User userEntity = userRepository.findById(user.getId()).orElseThrow();
-		LOV activeUserRole = sessionService.getSessionUserRole();
+        IUser<Long> user = sessionService.getSessionUser();
+        User userEntity = userRepository.findById(user.getId()).orElseThrow();
+        LOV activeUserRole = sessionService.getSessionUserRole();
+        SimpleDictionary filterByRangeEnabled = new SimpleDictionary("filterByRangeEnabled", String.valueOf(metaConfigurationProperties.isFilterByRangeEnabled()));
+        List<SimpleDictionary> featureSettingsList = new ArrayList<>();
+        featureSettingsList.add(filterByRangeEnabled);
+        return LoggedUser.builder()
+                .sessionId(sessionService.getSessionId())
+                .activeRole(activeUserRole.getKey())
+                .roles(userRoleService.getUserRoles(userEntity))
+                .screens(screenResponsibilityService.getScreens(user, activeUserRole))
+                .userSettingsVersion(null)
+                .lastName(userEntity.getLastName())
+                .firstName(userEntity.getFirstName())
+                .patronymic(userEntity.getPatronymic())
+                .fullName(userEntity.getFullName())
+                .principalName(userEntity.getUserPrincipalName())
+                .phone(userEntity.getPhone())
+                .featureSettings(this.getFeatureSettings())
+                .systemUrl(uiProperties.getSystemUrl())
+                .language(LocaleContextHolder.getLocale().getLanguage())
+                .timezone(LocaleContextHolder.getTimeZone().getID())
+                .devPanelEnabled(metaConfigurationProperties.isDevPanelEnabled())
+                .featureSettings(featureSettingsList)
+                .build();
 
-		return LoggedUser.builder()
-				.sessionId(sessionService.getSessionId())
-				.activeRole(activeUserRole.getKey())
-				.roles(userRoleService.getUserRoles(userEntity))
-				.screens(screenResponsibilityService.getScreens(user, activeUserRole))
-				.userSettingsVersion(null)
-				.lastName(userEntity.getLastName())
-				.firstName(userEntity.getFirstName())
-				.patronymic(userEntity.getPatronymic())
-				.fullName(userEntity.getFullName())
-				.principalName(userEntity.getUserPrincipalName())
-				.phone(userEntity.getPhone())
-				.featureSettings(this.getFeatureSettings())
-				.systemUrl(uiProperties.getSystemUrl())
-				.language(LocaleContextHolder.getLocale().getLanguage())
-				.timezone(LocaleContextHolder.getTimeZone().getID())
-				.devPanelEnabled(metaConfigurationProperties.isDevPanelEnabled())
-				.filterByRangeEnabled(metaConfigurationProperties.isFilterByRangeEnabled())
-				.build();
+    }
 
-	}
+    public void setSessionUserInternalRole(String role) {
+        var userDetails = coreSessionService.getSessionUserDetails(true);
+        if (role == null || role.isEmpty() || userDetails == null) {
+            return;
+        }
+        User user = userRepository.getById(userDetails.getId());
 
-	public void setSessionUserInternalRole(String role) {
-		var userDetails = coreSessionService.getSessionUserDetails(true);
-		if (role == null || role.isEmpty() || userDetails == null) {
-			return;
-		}
-		User user = userRepository.getById(userDetails.getId());
+        LOV userRole = new LOV(role);
+        userDetails.setUserRole(userRole);
+        userRoleService.updateMainUserRole(user, userRole);
+    }
 
-		LOV userRole = new LOV(role);
-		userDetails.setUserRole(userRole);
-		userRoleService.updateMainUserRole(user, userRole);
-	}
-
-	/**
-	 * Get available application features, e.g. comments/notification polling or supression of system errors
-	 * No implementation is provided by Cxbox UI by default so for now it is considered as a customization joint.
-	 *
-	 * @return Dictionary of string key and value (boolean)
-	 * Following keys were supported historically: FEATURE_COMMENTS, FEATURE_NOTIFICATIONS, FEATURE_HIDE_SYSTEM_ERRORS
-	 */
-	public Collection<SimpleDictionary> getFeatureSettings() {
-		return new ArrayList<>();
-	}
+    /**
+     * Get available application features, e.g. comments/notification polling or supression of system errors
+     * No implementation is provided by Cxbox UI by default so for now it is considered as a customization joint.
+     *
+     * @return Dictionary of string key and value (boolean)
+     * Following keys were supported historically: FEATURE_COMMENTS, FEATURE_NOTIFICATIONS, FEATURE_HIDE_SYSTEM_ERRORS
+     */
+    public Collection<SimpleDictionary> getFeatureSettings() {
+        return new ArrayList<>();
+    }
 }
