3722accb36ce2eb6fda2ee44e726006d535642d6
avgelfman <143416914+avgelfman@users.noreply.github.com>
2024-08-07 11:59:18 +0300

    CXBOX-84   Customization of displayed columns (#152)

--- src/main/java/org/demo/documentation/feature/customizationcolumns/MyEntity3135.java
+++ src/main/java/org/demo/documentation/feature/customizationcolumns/MyEntity3135.java
@@ -2,10 +2,15 @@ package org.demo.documentation.feature.customizationcolumns;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.cxbox.model.core.entity.BaseEntity;
+import org.demo.documentation.feature.customizationcolumns.enums.CustomFieldDictionaryEnum;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,4 +19,13 @@ import org.cxbox.model.core.entity.BaseEntity;
 public class MyEntity3135 extends BaseEntity {
     @Column
     private String customField;
+    @Column
+    private String customFieldInput;
+    @Column
+    private LocalDateTime customFieldDate;
+    @Column
+    private Boolean customFieldCheckBox;
+    @Enumerated(value = EnumType.STRING)
+    @Column
+    private CustomFieldDictionaryEnum customFieldDictionary;
 }
