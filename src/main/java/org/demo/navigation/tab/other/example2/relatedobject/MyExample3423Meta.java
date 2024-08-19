2c41269f33e53efb6c521dffea8619b75e189c75
avgelfman <143416914+avgelfman@users.noreply.github.com>
2024-08-19 16:40:11 +0300

    CXBOX-454 tab (#172)

--- src/main/java/org/demo/navigation/tab/other/example2/relatedobject/MyExample3423Meta.java
+++ src/main/java/org/demo/navigation/tab/other/example2/relatedobject/MyExample3423Meta.java
@@ -20,7 +20,7 @@ public class MyExample3423Meta extends FieldMetaBuilder<MyExample3423DTO> {
         fields.setDrilldown(
                 MyExample3423DTO_.customField,
                 DrillDownType.INNER,
-                "/screen/myexample3422/view/agreementInfo/" + CxboxMyExample3422Controller.myexample3423 + "/" + id
+                "/screen/myexample3422/view/agreementInfo/"+ CxboxMyExample3422Controller.myexample3422 +"/"+ parentId +"/"+ CxboxMyExample3422Controller.myexample3423 + "/" + id
         );
     }
 
