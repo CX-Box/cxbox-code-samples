365e04dab711cb581240d84633adb7a616a4cea7
kotman24 <andreyzaharenko81@gmail.com>
2024-09-04 12:12:23 +0300

    CXBOX-172 | MyExample162DTO.customField pole added SearchParameter annotation

--- src/main/java/org/demo/documentation/fields/hint/filtration/MyExample162DTO.java
+++ src/main/java/org/demo/documentation/fields/hint/filtration/MyExample162DTO.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.cxbox.api.data.dto.DataResponseDTO;
+import org.cxbox.core.util.filter.SearchParameter;
+import org.cxbox.core.util.filter.provider.impl.StringValueProvider;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class MyExample162DTO extends DataResponseDTO {
 
+    @SearchParameter(name = "customField", provider = StringValueProvider.class)
 	private String customField;
 
 	public MyExample162DTO(MyEntity162 entity) {
