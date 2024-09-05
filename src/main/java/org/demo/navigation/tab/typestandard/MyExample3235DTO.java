31e9de429be9ee4c2d61fc4d6eaa1bab067d6699
agelfman <agelfman@cxlab.ru>
2024-09-05 16:11:16 +0300

    CXBOX-269 Tabs

--- src/main/java/org/demo/navigation/tab/typestandard/MyExample3235DTO.java
+++ src/main/java/org/demo/navigation/tab/typestandard/MyExample3235DTO.java
@@ -4,12 +4,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Getter;
 import org.cxbox.api.data.dto.DataResponseDTO;
+import org.cxbox.core.util.filter.SearchParameter;
+import org.cxbox.core.util.filter.provider.impl.StringValueProvider;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class MyExample3235DTO extends DataResponseDTO {
 
+    @SearchParameter(name = "customField", provider = StringValueProvider.class)
     private String customField;
 
     public MyExample3235DTO(MyEntity3235 entity) {
