c66ae4709a7db0b8cb9e54f84dbb537423fd40d0
agelfman <agelfman@cxlab.ru>
2024-11-11 15:35:01 +0300

    CXBOX-451 LOV Icon

--- src/main/java/org/demo/documentation/fields/dictionary/dictionarylov/AdministeredDictionaryType.java
+++ src/main/java/org/demo/documentation/fields/dictionary/dictionarylov/AdministeredDictionaryType.java
@@ -3,6 +3,7 @@ package org.demo.documentation.fields.dictionary.dictionarylov;
 import static org.cxbox.api.data.dictionary.DictionaryCache.dictionary;
 
 import java.io.Serializable;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.cxbox.api.data.dictionary.IDictionaryType;
@@ -12,25 +13,31 @@ import org.cxbox.api.data.dictionary.LOV;
 @RequiredArgsConstructor
 public enum AdministeredDictionaryType implements Serializable, IDictionaryType {
 
-	REGIONS;
+    REGIONS;
 
-	@Override
-	public LOV lookupName(String val) {
-		return dictionary().lookupName(val, this);
-	}
+    @Override
+    public LOV lookupName(String val) {
+        return dictionary().lookupName(val, this);
+    }
 
-	@Override
-	public String lookupValue(LOV lov) {
-		return dictionary().lookupValue(lov, this);
-	}
+    @Override
+    public String lookupValue(LOV lov) {
+        return dictionary().lookupValue(lov, this);
+    }
 
-	@Override
-	public String getName() {
-		return name();
-	}
+    @Override
+    public String getName() {
+        return name();
+    }
 
-	public boolean containsKey(String key) {
-		return dictionary().containsKey(key, this);
-	}
+    public boolean containsKey(String key) {
+        return dictionary().containsKey(key, this);
+    }
 
+    public static class Regions {
+        public static final LOV MOSCOW = new LOV("MOSCOW");
+        public static final LOV SAINT_PETERBURG = new LOV("SAINT_PETERBURG");
+        public static final LOV SYKTYVKAR = new LOV("SYKTYVKAR");
+        public static final LOV KOSTROMA = new LOV("KOSTROMA");
+    }
 }
--- src/main/java/org/demo/documentation/fields/dictionary/dictionarylov/icon/MyExample351Meta.java
+++ src/main/java/org/demo/documentation/fields/dictionary/dictionarylov/icon/MyExample351Meta.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 
 import static org.demo.documentation.fields.dictionary.dictionarylov.AdministeredDictionaryType.REGIONS;
+import static org.demo.documentation.fields.dictionary.dictionarylov.AdministeredDictionaryType.Regions.*;
 import static org.demo.documentation.fields.dictionary.dictionarylov.icon.enums.IconsEnum.ARROW_UP;
 import static org.demo.documentation.fields.dictionary.dictionarylov.icon.enums.IconsEnum.DOWN;
 import static org.demo.documentation.fields.dictionary.icon.enums.IconsEnum.WATERMELON;
@@ -21,10 +22,11 @@ public class MyExample351Meta extends FieldMetaBuilder<MyExample351DTO> {
 
     @Override
     public void buildRowDependentMeta(RowDependentFieldsMeta<MyExample351DTO> fields, InnerBcDescription bcDescription,
-                                      Long id, Long parentId) {;
+                                      Long id, Long parentId) {
+        ;
         fields.setEnabled(MyExample351DTO_.customFieldDictionary);
         fields.setEnabled(MyExample351DTO_.customField);
-        fields.setDictionaryTypeWithAllValues(MyExample351DTO_.customFieldDictionary,REGIONS);
+        fields.setDictionaryTypeWithAllValues(MyExample351DTO_.customFieldDictionary, REGIONS);
     }
 
     @Override
@@ -32,11 +34,10 @@ public class MyExample351Meta extends FieldMetaBuilder<MyExample351DTO> {
         fields.enableFilter(MyExample351DTO_.customFieldDictionary);
         fields.enableSort(MyExample351DTO_.customFieldDictionary);
         Map<LOV, Icon> valueIconMap = Map.of(
-                //поправить на администрирования типов
-                REGIONS.lookupName("MOSCOW"), ARROW_UP,
-                REGIONS.lookupName("SAINT PETERBURG"), DOWN,
-                REGIONS.lookupName("SYKTYVKAR"), WATERMELON,
-                REGIONS.lookupName("KOSTROMA"), WATERMELON);
+                MOSCOW, ARROW_UP,
+                SAINT_PETERBURG, DOWN,
+                SYKTYVKAR, WATERMELON,
+                KOSTROMA, WATERMELON);
         fields.setAllValuesWithIcons(MyExample351DTO_.customFieldDictionary, REGIONS, valueIconMap);
         fields.setAllFilterValuesByLovType(MyExample351DTO_.customFieldDictionary, REGIONS);
     }
