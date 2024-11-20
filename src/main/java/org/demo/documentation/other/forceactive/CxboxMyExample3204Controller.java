dab12955e3217c125871312adc0f4dc84fae3f19
agelfman <agelfman@cxlab.ru>
2024-11-20 15:29:55 +0300

    CXBOX-534 Documentation.Description Grouping Hierarchy

---
+++ src/main/java/org/demo/documentation/other/forceactive/CxboxMyExample3204Controller.java
@@ -0,0 +1,57 @@
+package org.demo.documentation.other.forceactive;
+
+import lombok.Getter;
+import org.cxbox.api.data.BcIdentifier;
+import org.cxbox.core.crudma.bc.EnumBcIdentifier;
+import org.cxbox.core.crudma.bc.impl.AbstractEnumBcSupplier;
+import org.cxbox.core.crudma.bc.impl.BcDescription;
+import org.springframework.stereotype.Component;
+
+@Getter
+public enum CxboxMyExample3204Controller implements EnumBcIdentifier {
+
+    // @formatter:on
+
+    myexample3204(MyExample3204Service.class);
+
+    // @formatter:on
+
+    public static final EnumBcIdentifier.Holder<CxboxMyExample3204Controller> Holder = new Holder<>(
+            CxboxMyExample3204Controller.class);
+
+    private final BcDescription bcDescription;
+
+    CxboxMyExample3204Controller(String parentName, Class<?> serviceClass, boolean refresh) {
+        this.bcDescription = buildDescription(parentName, serviceClass, refresh);
+    }
+
+    CxboxMyExample3204Controller(String parentName, Class<?> serviceClass) {
+        this(parentName, serviceClass, false);
+    }
+
+    CxboxMyExample3204Controller(BcIdentifier parent, Class<?> serviceClass, boolean refresh) {
+        this(parent == null ? null : parent.getName(), serviceClass, refresh);
+    }
+
+    CxboxMyExample3204Controller(BcIdentifier parent, Class<?> serviceClass) {
+        this(parent, serviceClass, false);
+    }
+
+    CxboxMyExample3204Controller(Class<?> serviceClass, boolean refresh) {
+        this((String) null, serviceClass, refresh);
+    }
+
+    CxboxMyExample3204Controller(Class<?> serviceClass) {
+        this((String) null, serviceClass, false);
+    }
+
+    @Component
+    public static class BcSupplier extends AbstractEnumBcSupplier<CxboxMyExample3204Controller> {
+
+        public BcSupplier() {
+            super(CxboxMyExample3204Controller.Holder);
+        }
+
+    }
+
+}
---
+++ src/main/java/org/demo/documentation/other/forceactive/MyEntity3204TestDataLoadService.java
@@ -0,0 +1,26 @@
+package org.demo.documentation.other.forceactive;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import org.cxbox.api.service.session.InternalAuthorizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyEntity3204TestDataLoadService {
+
+    @Autowired
+    MyEntity3204Repository repository;
+
+    @Autowired
+    InternalAuthorizationService authzService;
+
+    @Transactional
+    @PostConstruct
+    public void load() {
+        authzService.loginAs(authzService.createAuthentication(InternalAuthorizationService.VANILLA));
+        repository.deleteAll();
+        repository.save(new MyEntity3204().setCustomField("test data").setCustomFieldDrillDown("Test"));
+    }
+
+}
