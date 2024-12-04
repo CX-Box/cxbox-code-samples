73b799c1c1e1134695a58105fc5df1919e0dbb33
agelfman <agelfman@cxlab.ru>
2024-12-04 10:18:54 +0300

    CXBOX-331 Description.Form popup

---
+++ src/main/java/org/demo/documentation/widgets/formpopup/fieldslayoute/MyExample3402Meta.java
@@ -0,0 +1,22 @@
+package org.demo.documentation.widgets.formpopup.fieldslayoute;
+
+import org.cxbox.core.crudma.bc.impl.InnerBcDescription;
+import org.cxbox.core.dto.rowmeta.FieldsMeta;
+import org.cxbox.core.dto.rowmeta.RowDependentFieldsMeta;
+import org.cxbox.core.service.rowmeta.FieldMetaBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyExample3402Meta extends FieldMetaBuilder<MyExample3402DTO> {
+
+    @Override
+    public void buildRowDependentMeta(RowDependentFieldsMeta<MyExample3402DTO> fields, InnerBcDescription bcDescription,
+                                      Long id, Long parentId) {
+        fields.setEnabled(MyExample3402DTO_.customField);
+    }
+
+    @Override
+    public void buildIndependentMeta(FieldsMeta<MyExample3402DTO> fields, InnerBcDescription bcDescription, Long parentId) {
+    }
+
+}
--- src/main/java/org/demo/documentation/widgets/list/showcondition/byparententity/MyEntity3100TestDataLoadService.java
+++ src/main/java/org/demo/documentation/widgets/list/showcondition/byparententity/MyEntity3100TestDataLoadService.java
@@ -27,12 +27,14 @@ public class MyEntity3100TestDataLoadService {
     @PostConstruct
     public void load() {
         authzService.loginAs(authzService.createAuthentication(InternalAuthorizationService.VANILLA));
-        repositoryParent.deleteAll();
         repositoryChild.deleteAll();
+        repositoryParent.deleteAll();
         MyEntity3100 myEntity3100Parent = new MyEntity3100().setCustomFieldNumber(8L).setCustomField("Test data");
+        MyEntity3100 myEntity3100Parent2 = new MyEntity3100().setCustomFieldNumber(8L).setCustomField("Test data");
         repositoryParent.save(myEntity3100Parent);
+        repositoryParent.save(myEntity3100Parent2);
         repositoryChild.save(new MyEntity3106().setCustomFieldEntity(myEntity3100Parent).setCustomField("Test data"));
-
+        repositoryChild.save(new MyEntity3106().setCustomFieldEntity(myEntity3100Parent).setCustomField("Test data2"));
     }
 
 }
