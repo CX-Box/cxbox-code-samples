a21c92a024d94f0f9825a6297bcbc73cb5062063
avgelfman <143416914+avgelfman@users.noreply.github.com>
2025-01-16 14:21:58 +0300

    CXBOX-746 Description by personal filter group (#323)
    * CXBOX-746 Description by personal filter group

--- src/main/java/org/demo/documentation/widgets/property/filtration/filtergroup/forpicklist/MyEntity3630PickDTO.java
+++ src/main/java/org/demo/documentation/widgets/property/filtration/filtergroup/forpicklist/MyEntity3630PickDTO.java
@@ -4,12 +4,15 @@ import org.cxbox.api.data.dto.DataResponseDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.cxbox.core.util.filter.SearchParameter;
+import org.cxbox.core.util.filter.provider.impl.StringValueProvider;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class MyEntity3630PickDTO extends DataResponseDTO {
 
+    @SearchParameter(name = "customField", provider = StringValueProvider.class)
     private String customField;
 
     public MyEntity3630PickDTO(MyEntity3630 entity) {
