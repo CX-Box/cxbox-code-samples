bec6cd1521e3dd6b94585c369591ec3628e0d981
agelfman <agelfman@cxlab.ru>
2025-01-23 11:19:14 +0300

    CXBOX-768 Description AssocListPopup

--- src/main/java/org/demo/documentation/widgets/property/filtration/fulltextsearch/forassoc/MyEntity3625Repository.java
+++ src/main/java/org/demo/documentation/widgets/property/filtration/fulltextsearch/forassoc/MyEntity3625Repository.java
@@ -1,8 +1,6 @@
 package org.demo.documentation.widgets.property.filtration.fulltextsearch.forassoc;
 
 import org.demo.conf.cxbox.extension.fulltextsearch.FullTextSearchExt;
-import org.demo.documentation.widgets.property.filtration.fulltextsearch.MyEntity3614;
-import org.demo.documentation.widgets.property.filtration.fulltextsearch.MyEntity3614_;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -10,5 +8,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MyEntity3625Repository extends JpaRepository<MyEntity3625, Long>, JpaSpecificationExecutor<MyEntity3625> {
+    default Specification<MyEntity3625> getCustomFieldLikeIgnoreCaseSpecification(String value) {
+        return (root, query, cb) -> FullTextSearchExt.likeIgnoreCase(value, cb, root.get(MyEntity3625_.customField));
+    }
 
 }
