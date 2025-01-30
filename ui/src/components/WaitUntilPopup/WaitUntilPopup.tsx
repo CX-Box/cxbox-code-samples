c1e7e44752bb8214456d96352fd866edfa400817
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2025-01-30 12:35:39 +0300

    CXBOX-447 | updated ui from demo branch

---
+++ ui/src/components/WaitUntilPopup/WaitUntilPopup.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback } from 'react'
+import { Modal, Spin } from 'antd'
+import { useAppSelector } from '@store'
+import { useTranslation } from 'react-i18next'
+import styles from './WaitUntilPopup.module.css'
+import Button from '@components/ui/Button/Button'
+import { useDispatch } from 'react-redux'
+import { actions } from '@actions'
+
+interface WaitUntilPopupProps {}
+
+function WaitUntilPopup(props: WaitUntilPopupProps) {
+    const { t } = useTranslation()
+
+    const popupData = useAppSelector(state => state.view.popupData)
+    const { type, options, bcName } = popupData || {}
+    const visibility = type === 'waitUntil'
+    const { message, status } = options || {}
+    const hasCompletionMessage = ['success', 'timeout'].includes(status as string) && message
+    const waiting = !hasCompletionMessage
+
+    const dispatch = useDispatch()
+    const handleOk = useCallback(() => {
+        dispatch(actions.closeViewPopup({ bcName }))
+    }, [bcName, dispatch])
+
+    return (
+        <div>
+            <Modal
+                className={styles.modal}
+                visible={visibility}
+                closable={false}
+                cancelText={null}
+                centered={true}
+                footer={null}
+                maskClosable={false}
+                maskStyle={{ backdropFilter: 'blur(50px)' }}
+            >
+                <div className={styles.content}>
+                    <span className={styles.title}>{t('Please, wait')}</span>
+                    <Spin className={styles.spin} spinning={waiting} size="large" />
+                    <span className={styles.text}>{message ?? t('Operation in progress')}</span>
+                </div>
+                <div className={styles.footer}>
+                    {!waiting && (
+                        <Button type="primary" onClick={handleOk}>
+                            {t('Ok')}
+                        </Button>
+                    )}
+                </div>
+            </Modal>
+        </div>
+    )
+}
+
+export default React.memo(WaitUntilPopup)
