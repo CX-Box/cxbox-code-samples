25a3a73f888d6554378cd38f8871bc1fa8dc9b5a
Алексей Н.В. <121115117+Lsplay2@users.noreply.github.com>
2025-02-13 13:38:04 +0400

    CXBOX-725.2 Fix Date

--- src/test/java/core/widget/modal/Calendar.java
+++ src/test/java/core/widget/modal/Calendar.java
@@ -1,9 +1,6 @@
 package core.widget.modal;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.*;
 import core.OriginExpectations.CxBoxExpectations;
 import io.qameta.allure.Step;
 import lombok.extern.slf4j.Slf4j;
