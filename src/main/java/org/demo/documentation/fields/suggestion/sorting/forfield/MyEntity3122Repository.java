b6f162cbbf83d5be135b9c51cb515f5886a1a81a
agelfman <agelfman@cxlab.ru>
2025-03-03 13:43:21 +0300

    CXBOX-780 Documention suggestionPickList

---
+++ src/main/java/org/demo/documentation/fields/suggestion/sorting/forfield/MyEntity3122Repository.java
@@ -0,0 +1,10 @@
+package org.demo.documentation.fields.suggestion.sorting.forfield;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyEntity3122Repository extends JpaRepository<MyEntity3122, Long>, JpaSpecificationExecutor<MyEntity3122> {
+
+}
