534e05996eae6352727dff7492b529015b817b67
Lsplay2 <lsplaywot8@yandex.ru>
2025-04-03 15:08:35 +0400

    CXBOX-876 Added java doc and removed regular expressions

--- src/test/java/core/widget/TestingTools/CustomAllureListener.java
+++ src/test/java/core/widget/TestingTools/CustomAllureListener.java
@@ -4,11 +4,21 @@ import io.qameta.allure.listener.TestLifecycleListener;
 import io.qameta.allure.model.TestResult;
 
 public class CustomAllureListener implements TestLifecycleListener {
+
+    /**
+     * The method transforms the standard allure (application.Samples.Form.HintOnFormTest.filtration)
+     * in a convenient mvn form (application.Samples.Form.HintOnFormTest#filtration)
+     * The method takes the result {result},
+     * extracts the full name of the path to the test from it,
+     * and changes the last one . on #
+     */
     @Override
     public void beforeTestStop(TestResult result) {
         String name = result.getFullName();
-        if (name.contains(".")) {
-            name = name.replaceFirst("\\.([^.]+)$", "#$1");
+        int lastDotIndex = name.lastIndexOf('.');
+        if (lastDotIndex != -1) {
+            name = name.substring(0, lastDotIndex) + "#" + name.substring(lastDotIndex + 1);
+
             result.setFullName(name);
         }
     }
