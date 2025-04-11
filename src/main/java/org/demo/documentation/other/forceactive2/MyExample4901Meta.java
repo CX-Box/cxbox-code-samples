e8fdb18c43b6dab79b46a6613c284e9194fe53ee
agelfman <agelfman@cxlab.ru>
2025-04-11 16:56:13 +0300

    CXBOX-812 ForceActive new tag

--- src/main/java/org/demo/documentation/other/forceactive2/MyExample4901Meta.java
+++ src/main/java/org/demo/documentation/other/forceactive2/MyExample4901Meta.java
@@ -8,31 +8,58 @@ import org.demo.documentation.other.forceactive2.enums.CountryEnum;
 import org.demo.documentation.other.forceactive2.enums.RegionEnum;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+
 @Service
 public class MyExample4901Meta extends FieldMetaBuilder<MyExample4901DTO> {
 
     @Override
     public void buildRowDependentMeta(RowDependentFieldsMeta<MyExample4901DTO> fields, InnerBcDescription bcDescription,
                                       Long id, Long parentId) {
+        fields.setEnabled(MyExample4901DTO_.street);
         fields.setEnabled(MyExample4901DTO_.money);
         fields.setEnabled(MyExample4901DTO_.descriptionProduct);
         fields.setEnabled(MyExample4901DTO_.product);
-        fields.setEnumValues(MyExample4901DTO_.country, CountryEnum.values());
-        if (fields.getCurrentValue(MyExample4901DTO_.country).isPresent()) {
-            if (CountryEnum.BELARUS.equals(fields.getCurrentValue(MyExample4901DTO_.country).get())) {
-                fields.setEnumValues(MyExample4901DTO_.region, RegionEnum.BREST, RegionEnum.GOMEL, RegionEnum.MINSK);
-            } else if (CountryEnum.RUSSIA.equals(fields.getCurrentValue(MyExample4901DTO_.country).get())) {
-                fields.setEnumValues(MyExample4901DTO_.region, RegionEnum.KOSTROMSKAYA, RegionEnum.MOSCOWSKAYA, RegionEnum.VOLGOGRADSKAYA);
-            }
-        }
         fields.setEnabled(MyExample4901DTO_.country);
-
         fields.setEnabled(MyExample4901DTO_.region);
         fields.setEnabled(MyExample4901DTO_.customField);
+
+        fields.setEnumValues(MyExample4901DTO_.country, CountryEnum.values());
+
+        if (fields.isFieldChangedNowFE(fields, MyExample4901DTO_.country)) {
+            if (fields.getCurrentValue(MyExample4901DTO_.country).isEmpty()) {
+                fields.setCurrentValue(MyExample4901DTO_.region, null);
+                fields.setCurrentValue(MyExample4901DTO_.street, null);
+            } else if (Objects.equals(fields.getCurrentValue(MyExample4901DTO_.country).orElse(null), CountryEnum.BELARUS)) {
+                fields.setEnumValues(MyExample4901DTO_.region, RegionEnum.BREST, RegionEnum.GOMEL, RegionEnum.MINSK);
+                fields.setCurrentValue(MyExample4901DTO_.region, RegionEnum.MINSK);
+            } else if (Objects.equals(fields.getCurrentValue(MyExample4901DTO_.country).orElse(null), CountryEnum.RUSSIA)) {
+                fields.setEnumValues(MyExample4901DTO_.region, RegionEnum.KOSTROMSKAYA, RegionEnum.MOSCOWSKAYA, RegionEnum.VOLGOGRADSKAYA);
+                fields.setCurrentValue(MyExample4901DTO_.region, RegionEnum.MOSCOWSKAYA);
+            }
+        }
+
+        if (fields.isFieldChangedNowFE(fields, MyExample4901DTO_.region)) {
+            if (fields.getCurrentValue(MyExample4901DTO_.region).isEmpty()) {
+                fields.setCurrentValue(MyExample4901DTO_.street, null);
+            } else if ((Objects.equals(fields.getCurrentValue(MyExample4901DTO_.region).orElse(null), RegionEnum.MINSK))) {
+                fields.setCurrentValue(MyExample4901DTO_.street, "Avenue Nezavisimosti");
+            } else if ((Objects.equals(fields.getCurrentValue(MyExample4901DTO_.region).orElse(null), RegionEnum.MOSCOWSKAYA))) {
+                fields.setCurrentValue(MyExample4901DTO_.street, "Tverskaya street");
+            }
+        }
+
+        if (fields.isFieldChangedNowFE(fields, MyExample4901DTO_.street)) {
+            fields.setCurrentValue(MyExample4901DTO_.customField, "New value");
+        }
     }
 
     @Override
-    public void buildIndependentMeta(FieldsMeta<MyExample4901DTO> fields, InnerBcDescription bcDescription, Long parentId) {
+    public void buildIndependentMeta(FieldsMeta<MyExample4901DTO> fields, InnerBcDescription bcDescription, Long
+            parentId) {
+        fields.enableFilter(MyExample4901DTO_.street);
         fields.enableFilter(MyExample4901DTO_.money);
         fields.enableFilter(MyExample4901DTO_.descriptionProduct);
         fields.enableFilter(MyExample4901DTO_.product);
