aea74519fa1552fb8baf479ff0cd140c4280ab99
avgelfman <143416914+avgelfman@users.noreply.github.com>
2025-05-20 21:41:44 +0300

    CXBOX-892 Add properties pagination nextAndPreviousWithCount,availableLimitsList (#436)

---
+++ src/main/java/org/demo/documentation/widgets/property/pagination/hidelimitoptions/MyEntity3865Repository.java
@@ -0,0 +1,10 @@
+package org.demo.documentation.widgets.property.pagination.hidelimitoptions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyEntity3865Repository extends JpaRepository<MyEntity3865, Long>, JpaSpecificationExecutor<MyEntity3865> {
+
+}
---
+++ src/main/java/org/demo/documentation/widgets/property/pagination/hidelimitoptions/MyEntity3865TestDataLoadService.java
@@ -0,0 +1,35 @@
+package org.demo.documentation.widgets.property.pagination.hidelimitoptions;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import org.cxbox.api.service.session.InternalAuthorizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyEntity3865TestDataLoadService {
+
+    @Autowired
+    MyEntity3865Repository repository;
+
+    @Autowired
+    InternalAuthorizationService authzService;
+
+    @Transactional
+    @PostConstruct
+    public void load() {
+        authzService.loginAs(authzService.createAuthentication(InternalAuthorizationService.VANILLA));
+        repository.deleteAll();
+        repository.save(new MyEntity3865().setCustomField("test data"));
+        repository.save(new MyEntity3865().setCustomField("test data7"));
+        repository.save(new MyEntity3865().setCustomField("test data3"));
+        repository.save(new MyEntity3865().setCustomField("test data4"));
+        repository.save(new MyEntity3865().setCustomField("test data5"));
+        repository.save(new MyEntity3865().setCustomField("test data6"));
+        repository.save(new MyEntity3865().setCustomField("test data7"));
+        repository.save(new MyEntity3865().setCustomField("test data8"));
+        repository.save(new MyEntity3865().setCustomField("test data9"));
+        repository.save(new MyEntity3865().setCustomField("test data10"));
+    }
+
+}
