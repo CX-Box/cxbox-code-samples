64072db6213eab0ece875f56769dc640b92c242c
IgorNB <igornb.mail@gmail.com>
2025-06-30 12:10:57 +0300

    CXBOX-698 | fix export error (#456)
    Co-authored-by: cxbox <cxbox.mail@gmail.com>

--- src/main/java/org/demo/documentation/fields/dictionary/dictionarydictionary/dictionary/MyExample357Service.java
+++ src/main/java/org/demo/documentation/fields/dictionary/dictionarydictionary/dictionary/MyExample357Service.java
@@ -1,5 +1,7 @@
 package org.demo.documentation.fields.dictionary.dictionarydictionary.dictionary;
 
+import static org.demo.documentation.fields.dictionary.dictionarydictionary.dictionary.DictionaryItemDTO_.*;
+
 import jakarta.persistence.EntityManager;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +12,7 @@ import org.cxbox.api.data.dto.DataResponseDTO;
 import org.cxbox.api.service.LocaleService;
 import org.cxbox.core.crudma.bc.BusinessComponent;
 import org.cxbox.core.crudma.impl.VersionAwareResponseService;
-import org.cxbox.core.dto.BusinessError;
+import org.cxbox.core.dto.BusinessError.Entity;
 import org.cxbox.core.dto.rowmeta.ActionResultDTO;
 import org.cxbox.core.dto.rowmeta.AssociateResultDTO;
 import org.cxbox.core.dto.rowmeta.CreateResult;
@@ -21,13 +23,14 @@ import org.cxbox.core.file.service.CxboxFileService;
 import org.cxbox.core.service.action.ActionScope;
 import org.cxbox.core.service.action.Actions;
 import org.cxbox.model.core.api.TranslationId;
+import org.cxbox.model.core.dao.JpaDao;
 import org.cxbox.model.dictionary.entity.DictionaryItem;
 import org.cxbox.model.dictionary.entity.DictionaryItemTranslation;
 import org.cxbox.model.dictionary.entity.DictionaryItem_;
 import org.cxbox.model.dictionary.entity.DictionaryTypeDesc;
+import org.demo.util.CSVUtils;
 import org.hibernate.exception.ConstraintViolationException;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
@@ -41,49 +44,48 @@ import java.util.stream.Collectors;
 @Service
 public class MyExample357Service extends VersionAwareResponseService<DictionaryItemDTO, DictionaryItem> {
 
+    private final DictionaryCache dictionaryCache;
+
+    private final JpaDao jpaDao;
+
+    private final EntityManager entityManager;
+
+    private final LocaleService localeService;
+
+    private final CxboxFileService cxboxFileService;
+
     @Getter(onMethod_ = @Override)
     private final Class<MyExample357Meta> meta = MyExample357Meta.class;
-    @Autowired
-    private DictionaryCache dictionaryCache;
 
-    private final MyEntity357Repository repository;
-    @Autowired
-    private EntityManager entityManager;
-
-    @Autowired
-    private LocaleService localeService;
-
-    @Autowired
-    private CxboxFileService cxboxFileService;
+    @Override
+    protected ActionResultDTO<DictionaryItemDTO> doUpdateEntity(DictionaryItem entity, DictionaryItemDTO data,
+        BusinessComponent bc) {
+        setIfChanged(data, type, entity::setType);
+        if (data.isFieldChanged(dictionaryTypeId)) {
+            entity.setDictionaryTypeId(data.getDictionaryTypeId() != null
+                ? entityManager.getReference(DictionaryTypeDesc.class, data.getDictionaryTypeId())
+                : null);
+        }
+        setIfChanged(data, key, entity::setKey);
+        if (data.isFieldChanged(value)) {
+            entity.setValue(data.getValue());
+            entity.getTranslations().forEach((lang, tr) -> tr.setValue(data.getValue()));
+        }
+        setIfChanged(data, active, entity::setActive);
+        setIfChanged(data, displayOrder, entity::setDisplayOrder);
+        setIfChanged(data, description, entity::setDescription);
+        return new ActionResultDTO<>(entityToDto(bc, entity));
+    }
 
     @Override
     protected CreateResult<DictionaryItemDTO> doCreateEntity(DictionaryItem entity, BusinessComponent bc) {
         entity.setActive(true);
         entity.setTranslations(localeService.getSupportedLanguages().stream().collect(Collectors.toMap(
-                lang -> lang,
-                lang -> new DictionaryItemTranslation(new TranslationId(lang), entity, null)
+            lang -> lang,
+            lang -> new DictionaryItemTranslation(new TranslationId(lang), entity, null)
         )));
-        repository.save(entity);
+        jpaDao.save(entity);
         return new CreateResult<>(entityToDto(bc, entity));
-    }
-    @Override
-    protected ActionResultDTO<DictionaryItemDTO> doUpdateEntity(DictionaryItem entity, DictionaryItemDTO data,
-                                                                BusinessComponent bc) {
-        setIfChanged(data, DictionaryItemDTO_.type, entity::setType);
-        if (data.isFieldChanged(DictionaryItemDTO_.dictionaryTypeId)) {
-            entity.setDictionaryTypeId(data.getDictionaryTypeId() != null
-                    ? entityManager.getReference(DictionaryTypeDesc.class, data.getDictionaryTypeId())
-                    : null);
-        }
-        setIfChanged(data, DictionaryItemDTO_.key, entity::setKey);
-        if (data.isFieldChanged(DictionaryItemDTO_.value)) {
-            entity.setValue(data.getValue());
-            entity.getTranslations().forEach((lang, tr) -> tr.setValue(data.getValue()));
-        }
-        setIfChanged(data, DictionaryItemDTO_.active, entity::setActive);
-        setIfChanged(data, DictionaryItemDTO_.displayOrder, entity::setDisplayOrder);
-        setIfChanged(data, DictionaryItemDTO_.description, entity::setDescription);
-        return new ActionResultDTO<>(entityToDto(bc, entity));
     }
 
     // --8<-- [start:updateEntity]
@@ -131,18 +133,20 @@ public class MyExample357Service extends VersionAwareResponseService<DictionaryI
     // --8<-- [start:validate]
     private void validate(BusinessComponent bc, ActionResultDTO<DictionaryItemDTO> result) {
         try {
-            repository.flush();
+            jpaDao.flush();
         } catch (DataIntegrityViolationException e) {
             if (e.getCause() instanceof ConstraintViolationException uniqEx) {
                 if (DictionaryItem.CONSTRAINT_UNIQ_TYPE_KEY.equalsIgnoreCase(uniqEx.getConstraintName())) {
-                    throw new BusinessException(e).setEntity(new BusinessError.Entity(bc).addField(
-                            DictionaryItem_.key.getName(),
-                            "Key already exists for type " + result.getRecord().getType()));
+                    throw new BusinessException(e).setEntity(new Entity(bc).addField(
+                        DictionaryItem_.key.getName(),
+                        "Key already exists for type " + result.getRecord().getType()
+                    ));
                 }
                 if (DictionaryItem.CONSTRAINT_UNIQ_TYPE_VALUE.equalsIgnoreCase(uniqEx.getConstraintName())) {
-                    throw new BusinessException(e).setEntity(new BusinessError.Entity(bc).addField(
-                            DictionaryItem_.value.getName(),
-                            "Value already exists for type " + result.getRecord().getType()));
+                    throw new BusinessException(e).setEntity(new Entity(bc).addField(
+                        DictionaryItem_.value.getName(),
+                        "Value already exists for type " + result.getRecord().getType()
+                    ));
                 }
             }
             throw e;
@@ -157,21 +161,21 @@ public class MyExample357Service extends VersionAwareResponseService<DictionaryI
     private FileDownloadDto toCsv() {
         String name = "DICTIONARY.csv";
         var header = List.of("TYPE", "KEY", "VALUE", "DISPLAY_ORDER", "DESCRIPTION", "ACTIVE", "ID");
-        var body = repository.findAll().stream()
-                .sorted(Comparator.comparing(DictionaryItem::getType)
-                        .thenComparing(dictionaryItem -> Optional.ofNullable(dictionaryItem.getDisplayOrder()).orElse(Integer.MAX_VALUE))
-                        .thenComparing(DictionaryItem::getValue)
-                        .thenComparing(DictionaryItem::getId)
-                )
-                .map(e -> List.of(
-                        e.getType(),
-                        e.getKey(),
-                        e.getValue(),
-                        Optional.ofNullable(e.getDisplayOrder()).map(d -> "" + d).orElse(""),
-                        e.getDescription(),
-                        e.isActive() ? "" : "false",
-                        ""
-                ));
+        var body = jpaDao.getList(DictionaryItem.class).stream()
+            .sorted(Comparator.comparing(DictionaryItem::getType)
+                .thenComparing(dictionaryItem -> Optional.ofNullable(dictionaryItem.getDisplayOrder()).orElse(Integer.MAX_VALUE))
+                .thenComparing(DictionaryItem::getValue)
+                .thenComparing(DictionaryItem::getId)
+            )
+            .map(e -> List.of(
+                Optional.ofNullable(e.getType()).orElse(""),
+                Optional.ofNullable(e.getKey()).orElse(""),
+                Optional.ofNullable(e.getValue()).orElse(""),
+                Optional.ofNullable(e.getDisplayOrder()).map(d -> "" + d).orElse(""),
+                Optional.ofNullable(e.getDescription()).orElse(""),
+                e.isActive() ? "" : "false",
+                ""
+            ));
         return CSVUtils.toCsv(header, body, name, ";");
     }
     // --8<-- [end:toCsv]
