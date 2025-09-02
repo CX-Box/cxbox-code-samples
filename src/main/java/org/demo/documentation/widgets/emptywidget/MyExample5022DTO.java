c4a4061ddd0836b9862b0659a3859c01559dee15
agelfman <agelfman@cxlab.ru>
2025-09-02 14:59:46 +0300

    CXBOX-1061 | Added. Description Header, empty Widget

--- src/main/java/org/demo/documentation/widgets/emptywidget/MyExample5022DTO.java
+++ src/main/java/org/demo/documentation/widgets/emptywidget/MyExample5022DTO.java
@@ -1,19 +1,17 @@
 package org.demo.documentation.widgets.emptywidget;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Getter;
 import org.cxbox.api.data.dto.DataResponseDTO;
 import org.cxbox.core.util.filter.SearchParameter;
-import org.cxbox.core.util.filter.provider.impl.StringValueProvider;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class MyExample5022DTO extends DataResponseDTO {
 
-    @SearchParameter(name = "customField", provider = StringValueProvider.class)
+    @SearchParameter(name = "customField")
     private String customField;
 
     public MyExample5022DTO(MyEntity5022 entity) {
