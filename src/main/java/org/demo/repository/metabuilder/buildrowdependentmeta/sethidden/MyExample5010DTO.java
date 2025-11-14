94e2d666031ff3af4235f52fda12d065a31d7207
agelfman <agelfman@cxlab.ru>
2025-11-14 16:48:24 +0300

    CXBOX-1043| Added. Add example for setHidden

---
+++ src/main/java/org/demo/repository/metabuilder/buildrowdependentmeta/sethidden/MyExample5010DTO.java
@@ -0,0 +1,29 @@
+package org.demo.repository.metabuilder.buildrowdependentmeta.sethidden;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import org.cxbox.api.data.dto.DataResponseDTO;
+import org.cxbox.core.util.filter.SearchParameter;
+import org.cxbox.core.util.filter.provider.impl.EnumValueProvider;
+import org.cxbox.core.util.filter.provider.impl.StringValueProvider;
+import org.demo.repository.metabuilder.buildrowdependentmeta.sethidden.enums.CustomFieldEnum;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MyExample5010DTO extends DataResponseDTO {
+
+
+    @SearchParameter(name = "customField", provider = EnumValueProvider.class)
+    private CustomFieldEnum customField;
+
+    @SearchParameter(name = "customFieldHidden", provider = StringValueProvider.class)
+    private String customFieldHidden;
+
+    public MyExample5010DTO(MyEntity5010 entity) {
+        this.id = entity.getId().toString();
+        this.customField = entity.getCustomField();
+        this.customFieldHidden = entity.getCustomFieldHidden();
+    }
+}
