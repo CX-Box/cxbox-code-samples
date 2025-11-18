20d062786ab332b8d131a4718d04eea7023a812c
agelfman <agelfman@cxlab.ru>
2025-11-18 12:58:08 +0300

    CXBOX-1046 | Fixed. Fix setFileAccept

--- src/main/java/org/demo/conf/cxbox/customization/dictionary/conf/DictionaryConfig.java
+++ src/main/java/org/demo/conf/cxbox/customization/dictionary/conf/DictionaryConfig.java
@@ -21,38 +21,49 @@ import org.cxbox.api.data.dictionary.DictionaryCache;
 import org.cxbox.api.data.dictionary.SimpleDictionary;
 import org.cxbox.dictionary.Dictionary;
 import org.cxbox.dictionary.DictionaryProvider;
+import org.demo.documentation.fields.dictionary.dictionarydictionary.sorting.CustomDictionarySortingExample;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Collection;
+import java.util.Comparator;
 
 @Configuration
 public class DictionaryConfig {
 
-	@Bean
-	public DictionaryProvider dictionaryProvider() {
-		return new DictionaryProvider() {
+    @Bean
+    public DictionaryProvider dictionaryProvider() {
+        return new DictionaryProvider() {
 
-			@Override
-			public <T extends Dictionary> T lookupName(@NonNull Class<T> type, @NonNull DictionaryValue value) {
-				var dictTmp = Dictionary.of(type, "");
-				var lov = DictionaryCache.dictionary().lookupName(value.getValue(), dictTmp.getDictionaryType());
-				return Dictionary.of(type, lov.getKey());
-			}
+            @Override
+            public <T extends Dictionary> T lookupName(@NonNull Class<T> type, @NonNull DictionaryValue value) {
+                var dictTmp = Dictionary.of(type, "");
+                var lov = DictionaryCache.dictionary().lookupName(value.getValue(), dictTmp.getDictionaryType());
+                return Dictionary.of(type, lov.getKey());
+            }
 
-			@Override
-			public <T extends Dictionary> SimpleDictionary lookupValue(@NonNull T dictionary) {
-				return DictionaryCache.dictionary().get(dictionary.getDictionaryType(), dictionary.key());
-			}
-
-			@Override
-			public <T extends Dictionary> Collection<T> getAll(@NonNull Class<T> dictionaryType) {
-				return DictionaryCache.dictionary().getAll(Dictionary.of(dictionaryType, "").getDictionaryType())
-						.stream()
-						.map(e -> Dictionary.of(dictionaryType, e.getKey()))
-						.toList();
-			}
-		};
-	}
+            @Override
+            public <T extends Dictionary> SimpleDictionary lookupValue(@NonNull T dictionary) {
+                return DictionaryCache.dictionary().get(dictionary.getDictionaryType(), dictionary.key());
+            }
+			// --8<-- [start:getAll]
+            @Override
+            public <T extends Dictionary> Collection<T> getAll(@NonNull Class<T> dictionaryType) {
+                if (dictionaryType == CustomDictionarySortingExample.class) {
+                    return DictionaryCache.dictionary()
+							.getAll(Dictionary.of(dictionaryType, "")
+							.getDictionaryType()).
+							stream()
+							.sorted(Comparator.comparing(SimpleDictionary::getKey))
+							.map(e -> Dictionary.of(dictionaryType, e.getKey())).toList();
+                }
+                return DictionaryCache.dictionary().getAll(Dictionary.of(dictionaryType, "").getDictionaryType())
+                        .stream()
+                        .map(e -> Dictionary.of(dictionaryType, e.getKey()))
+                        .toList();
+            }
+			// --8<-- [end:getAll]
+        };
+    }
 
 }
