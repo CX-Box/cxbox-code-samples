d42823b9df23072e0bab163be4e62b5b46f67b55
avgelfman <143416914+avgelfman@users.noreply.github.com>
2025-11-24 14:27:01 +0300

    CXBOX-1132 | Added. Add PDQ with hidden field (#570)
    * CXBOX-1132 | Added. Add PDQ with hidden field

---
+++ src/main/java/org/demo/documentation/widgets/property/filtration/filtergrouphiddenfield/MyExample3628Meta.java
@@ -0,0 +1,29 @@
+package org.demo.documentation.widgets.property.filtration.filtergrouphiddenfield;
+
+import org.cxbox.core.crudma.bc.impl.InnerBcDescription;
+import org.cxbox.core.dto.rowmeta.FieldsMeta;
+import org.cxbox.core.dto.rowmeta.RowDependentFieldsMeta;
+import org.cxbox.core.service.rowmeta.FieldMetaBuilder;
+import org.demo.documentation.widgets.property.filtration.filtergrouphiddenfield.enums.CustomFieldDictionaryEnum;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyExample3628Meta extends FieldMetaBuilder<MyExample3628DTO> {
+
+    @Override
+    public void buildRowDependentMeta(RowDependentFieldsMeta<MyExample3628DTO> fields, InnerBcDescription bcDescription,
+                                      Long id, Long parentId) {
+      fields.setEnabled(MyExample3628DTO_.customFieldCheckbox);
+      fields.setEnumValues(MyExample3628DTO_.customFieldDictionary, CustomFieldDictionaryEnum.values());
+      fields.setEnabled(MyExample3628DTO_.customFieldDictionary);
+      fields.setEnabled(MyExample3628DTO_.customField);
+    }
+
+    @Override
+    public void buildIndependentMeta(FieldsMeta<MyExample3628DTO> fields, InnerBcDescription bcDescription, Long parentId) {
+      fields.enableFilter(MyExample3628DTO_.customFieldCheckbox);
+      fields.setEnumFilterValues(fields, MyExample3628DTO_.customFieldDictionary, CustomFieldDictionaryEnum.values());
+      fields.enableFilter(MyExample3628DTO_.customFieldDictionary);
+    }
+
+}
