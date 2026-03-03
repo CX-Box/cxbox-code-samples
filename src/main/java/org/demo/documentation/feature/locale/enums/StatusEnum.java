2c884638fa6d40b5a331cf0111e9b213e7495b0b
agelfman <agelfman@cxlab.ru>
2026-03-03 13:13:35 +0300

    CXBOX-1248 | Added. Added locale

--- src/main/java/org/demo/documentation/feature/locale/enums/StatusEnum.java
+++ src/main/java/org/demo/documentation/feature/locale/enums/StatusEnum.java
@@ -1,15 +1,12 @@
 package org.demo.documentation.feature.locale.enums;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import com.fasterxml.jackson.annotation.JsonValue;
-import org.demo.conf.locale.LocalizedEnum;
-import org.demo.conf.locale.LocalizedEnumUtil;
+import org.demo.conf.locale.LocaleEnum;
 
 @Getter
 @AllArgsConstructor
-public enum StatusEnum implements LocalizedEnum {
+public enum StatusEnum implements LocaleEnum {
 
 	NEW("New", "Nouvelle"),
 	INACTIVE("Inactive", "Inactive"),
@@ -19,16 +16,4 @@ public enum StatusEnum implements LocalizedEnum {
 	private final String value;
 
 	private final String valueFr;
-
-	@JsonValue
-	public String toValue() {
-		return LocalizedEnumUtil.toValue(this);
-	}
-
-	@JsonCreator
-	public static StatusEnum fromValue(String value) {
-		return LocalizedEnumUtil
-				.fromValue(StatusEnum.class, value)
-				.orElse(null);
-	}
 }
