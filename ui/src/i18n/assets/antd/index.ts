95af833569f7a37a02be47bed1fc42d69eed2f46
agelfman <agelfman@cxlab.ru>
2026-03-12 12:52:30 +0300

    CXBOX-1248 | Added. Added locale

--- ui/src/i18n/assets/antd/index.ts
+++ ui/src/i18n/assets/antd/index.ts
@@ -4,4 +4,4 @@ import enUs from 'antd/es/locale-provider/en_US'
 import ruRu from 'antd/es/locale-provider/ru_RU'
 import frFr from 'antd/es/locale-provider/fr_FR'
 
-export default { en: enUs, ru: ruRu, fr: frFr } as { [key in SupportedLanguage]: Locale }
+export default { en: enUs, ru: ruRu, fr: frFr } as Record<SupportedLanguage, Locale>
