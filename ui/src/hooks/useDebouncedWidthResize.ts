c21465346b1451c66ed5e7420e9e54e0c6d7eb45
cxbox <cxbox.mail@gmail.com>
2026-07-20 19:45:54 +0300

    feature/CXBOX-1364

---
+++ ui/src/hooks/useDebouncedWidthResize.ts
@@ -0,0 +1,57 @@
+import { RefObject, useCallback, useRef, useEffect } from 'react'
+import { useResizeObserver } from '@hooks/useResizeObserver'
+
+export const useDebouncedWidthResize = (
+    ref: RefObject<HTMLElement>,
+    onResize: (width: number, entry: ResizeObserverEntry) => void,
+    delay: number = 100,
+    tolerance: number = 1
+) => {
+    const prevWidth = useRef<number>(0)
+    const resizeTimer = useRef<ReturnType<typeof setTimeout>>()
+    const onResizeRef = useRef(onResize)
+
+    useEffect(() => {
+        onResizeRef.current = onResize
+    }, [onResize])
+
+    const handleResize = useCallback<ResizeObserverCallback>(
+        entries => {
+            const entry = entries?.[0]
+
+            if (!entry) {
+                return
+            }
+
+            const width = entry.contentRect.width
+
+            // Ignore changes if the width has not changed.
+            if (Math.abs(prevWidth.current - width) < tolerance) {
+                return
+            }
+
+            prevWidth.current = width
+
+            if (resizeTimer.current) {
+                clearTimeout(resizeTimer.current)
+            }
+
+            resizeTimer.current = setTimeout(() => {
+                window.requestAnimationFrame(() => {
+                    onResizeRef.current(width, entry)
+                })
+            }, delay)
+        },
+        [delay, tolerance]
+    )
+
+    useResizeObserver(ref, handleResize)
+
+    useEffect(() => {
+        return () => {
+            if (resizeTimer.current) {
+                clearTimeout(resizeTimer.current)
+            }
+        }
+    }, [])
+}
