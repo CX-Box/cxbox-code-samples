380909211b585ddc3773a8f8227d7294c9319f1a
dpesternikov <dpesternikov@phoenixit.ru>
2026-08-07 12:26:14 +0300

    add: multi-file upload support to ListWidget

--- src/test/java/core/element/widget/list/ListWidget.java
+++ src/test/java/core/element/widget/list/ListWidget.java
@@ -5,6 +5,9 @@ import core.element.widget.PlatformWidget;
 import core.element.widget.action.Action;
 import core.element.widget.list.rows.PlatformRows;
 import core.element.widget.list.rows.row.PlatformRow;
+import io.qameta.allure.Allure;
+import java.io.File;
+import org.testcontainers.shaded.com.google.errorprone.annotations.CanIgnoreReturnValue;
 
 public abstract class ListWidget<SELF extends ListWidget<SELF, ROWS, ROW>, ROWS extends PlatformRows<ROWS, SELF, ROW>, ROW extends PlatformRow<ROW, ROWS, SELF>> extends PlatformWidget<SELF> {
 
@@ -31,4 +34,39 @@ public abstract class ListWidget<SELF extends ListWidget<SELF, ROWS, ROW>, ROWS 
 		return new Action<>(self());
 	}
 
+	@CanIgnoreReturnValue
+	public SELF uploadFilesViaDragAndDrop(File... files) {
+		return Allure.step("Uploading multiple files via Drag & Drop zone", step -> {
+			logTime(step);
+			element().$(".ant-upload-drag input[type='file']").uploadFile(files);
+			return self();
+		});
+	}
+
+	@CanIgnoreReturnValue
+	public SELF uploadFilesViaDragAndDropFromClasspath(String... fileNames) {
+		return Allure.step("Uploading multiple files via Drag & Drop zone from classpath", step -> {
+			logTime(step);
+			element().$(".ant-upload-drag input[type='file']").uploadFromClasspath(fileNames);
+			return self();
+		});
+	}
+
+	@CanIgnoreReturnValue
+	public SELF uploadFilesViaButton(File... files) {
+		return Allure.step("Uploading multiple files via action button", step -> {
+			logTime(step);
+			element().$(".ant-upload-select input[type='file']").uploadFile(files);
+			return self();
+		});
+	}
+
+	@CanIgnoreReturnValue
+	public SELF uploadFilesViaButtonFromClasspath(String... fileNames) {
+		return Allure.step("Uploading multiple files via action button from classpath", step -> {
+			logTime(step);
+			element().$(".ant-upload-select input[type='file']").uploadFromClasspath(fileNames);
+			return self();
+		});
+	}
 }
