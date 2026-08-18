3f641cfb023d647c5e442b962cb0c377f170e139
agelfman <agelfman@cxlab.ru>
2026-08-18 13:54:13 +0300

    CXBOX-1341 | Fixed.Fixed parent_id

--- src/main/java/org/demo/documentation/widgets/tree/data/departments/MydepartmensRepository.java
+++ src/main/java/org/demo/documentation/widgets/tree/data/departments/MydepartmensRepository.java
@@ -22,18 +22,54 @@ public interface MydepartmensRepository extends JpaRepository<Mydepartments, Lon
 				-> FullTextSearchExt.likeIgnoreCase(value, cb, root.get(Mydepartments_.departmentName));
 	}
 	@Query("""
-		SELECT CONCAT(mydept.id, '-', u.id), 
+		SELECT CONCAT(mydept.id, '-', COALESCE(u.id, 0)), 
 				 mydept.parentId, 
 				mydept.departmentName, 
 				u.lastName, u.firstName, u.middleName,
 				 CONCAT(u.lastName, ' ', u.firstName, ' ', u.middleName) AS fullName,
-				 CASE WHEN (SELECT COUNT(c) FROM Mydepartments c WHERE c.parentId = mydept.id) = 0
-                    THEN true ELSE false END AS isLeaf
+				 case when  mydept.parentId is null  then FALSE else TRUE end AS isLeaf
 		FROM Mydepartments mydept
-		JOIN mydept.fullNameList u
+		LEFT JOIN mydept.fullNameList u
 		ORDER BY mydept.id, u.id
 		OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY
 		""")
 	List<DepartmentUsersPrj> allDepartmentUsers(@Param("offset") int offset, @Param("limit") int limit);
 
+	@Query("""
+    SELECT CONCAT(mydept.id, '-', COALESCE(u.id, 0)) AS Id,
+           mydept.parentId AS parentId,
+           mydept.departmentName AS departmentName,
+           u.lastName AS lastName,
+           u.firstName AS firstName,
+           u.middleName AS middleName,
+           CONCAT(u.lastName, ' ', u.firstName, ' ', u.middleName) AS fullName,
+           CASE WHEN mydept.parentId IS NULL THEN FALSE ELSE TRUE END AS isLeaf
+    FROM Mydepartments mydept
+    LEFT JOIN mydept.fullNameList u
+	WHERE (CASE WHEN mydept.parentId IS NULL THEN FALSE ELSE TRUE END) = :isLeaf
+		ORDER BY mydept.id, u.id
+		OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY
+	""")
+	List<DepartmentUsersPrj> allDepartmentUsersisLeaf(@Param("offset") int offset,
+	                                                  @Param("limit") int limit,
+	                                                  @Param("isLeaf") boolean isLeaf);
+
+	@Query("""
+    SELECT CONCAT(mydept.id, '-', COALESCE(u.id, 0)) AS Id,
+           mydept.parentId AS parentId,
+           mydept.departmentName AS departmentName,
+           u.lastName AS lastName,
+           u.firstName AS firstName,
+           u.middleName AS middleName,
+           CONCAT(u.lastName, ' ', u.firstName, ' ', u.middleName) AS fullName,
+           CASE WHEN mydept.parentId IS NULL THEN FALSE ELSE TRUE END AS isLeaf
+    FROM Mydepartments mydept
+    LEFT JOIN mydept.fullNameList u
+    WHERE mydept.id = :deptId
+    ORDER BY mydept.id, u.id
+    OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY
+""")
+	List<DepartmentUsersPrj> allDepartmentUsersDeptId(@Param("offset") int offset,
+	                                                  @Param("limit") int limit,
+	                                                  @Param("deptId") String deptId);
 }
