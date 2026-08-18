af264d9706a1b228147ecb3df447d3fc190bcbde
agelfman <agelfman@cxlab.ru>
2026-08-18 14:51:33 +0300

    CXBOX-1341 | Fixed.Fixed parent_id

--- src/main/java/org/demo/documentation/widgets/tree/data/departments/DepartmentUsersPrj.java
+++ src/main/java/org/demo/documentation/widgets/tree/data/departments/DepartmentUsersPrj.java
@@ -1,14 +1,16 @@
 package org.demo.documentation.widgets.tree.data.departments;
 
+import lombok.Builder;
+
+@Builder
 public record DepartmentUsersPrj(
 		String id,
-		Long parentId,
+		String parentId,
 		String departmentName,
 		String lastName,
 		String firstName,
 		String middleName,
 		String fullName,
 		Boolean isLeaf
-) {
+) { }
 
-}
