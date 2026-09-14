24b1c67b734f59a91e8c1d9211d0ed58dc473792
cxbox <cxbox.mail@gmail.com>
2026-09-14 17:41:08 +0300

    CXBOX-1281 | Fixed. Hint samples: color, drilldown, filtration by contains; tests with doc shots; fb.hint in drilldown filter sample; core 5.0.3-SNAPSHOT
    (cherry picked from commit 6f6c7385576948654c3eb50f0688a9c92149451f)

--- src/test/java/application/Samples/Info/HintOnInfoTest.java
+++ src/test/java/application/Samples/Info/HintOnInfoTest.java
@@ -3,10 +3,11 @@ package application.Samples.Info;
 import application.config.BaseTestForSamples;
 import application.custom.Position;
 import core.element.PlatformApp;
+import core.util.DocShots;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -18,15 +19,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Info. Checking the basic functions for the Hint")
 @Epic("Samples")
+@Feature(HintOnInfoTest.ARTICLE)
 @Tag("Samples")
 public class HintOnInfoTest extends BaseTestForSamples {
 
-	@Disabled
+	static final String ARTICLE = "widget/fields/field/hint";
+
 	@Test
 	@Tag("Positive")
 	@DisplayName("A test to get the field color value in Hex format")
-	@Description("InfoWidget does not have a Hex field color for this type.")
+	@Description("The test gets the value from the style attribute in RGB format, and then converts it to Hex format")
 	void color() {
+		var info = PlatformApp.screen("Hint color")
+				.secondLevelView("Info")
+				.info("Info title");
+		info.hint("Custom Field")
+				.checkColor(color -> assertThat(color).isEqualTo("#EDA6A6"));
+		DocShots.png(info.element(), ARTICLE, "img_color_info.png", 1600, 1000);
 	}
 
 	@Disabled
@@ -46,12 +55,17 @@ public class HintOnInfoTest extends BaseTestForSamples {
 	void filtration() {
 	}
 
-	@Disabled
 	@Test
 	@Tag("Positive")
 	@DisplayName("The DrillDown test")
-	@Description("Drill Down is not available for InfoWidget.")
+	@Description("Checking the url before the transition and after the transition/click on a special element")
 	void drillDown() {
+		var info = PlatformApp.screen("Hint drilldown")
+				.secondLevelView("Info")
+				.info("Info title");
+		DocShots.png(info.element(), ARTICLE, "img_drilldown_info.png", 1600, 1000);
+		info.hint("Custom Field")
+				.drilldown(url -> assertThat(url).contains("/screen/myexample161/view/myexample161form/myExampleBc161/"));
 	}
 
 	@Disabled
@@ -72,6 +86,7 @@ public class HintOnInfoTest extends BaseTestForSamples {
 				.info("Info title");
 		info.hint("Custom Field")
 				.checkValue(val -> assertThat(val).isEqualTo("Information data"));
+		DocShots.png(info.element(), ARTICLE, "img_info.png", 1600, 1000);
 	}
 
 	@Test
